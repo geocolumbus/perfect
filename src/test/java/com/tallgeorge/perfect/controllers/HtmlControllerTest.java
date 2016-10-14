04507e1fdfbf54b2ffccb9a3986bafe2ce8e54a2
George Campbell <campbelg@oclc.org>
2016-10-14 16:22:05 -0400

    Update test.

--- src/test/java/com/tallgeorge/perfect/controllers/HtmlControllerTest.java
+++ src/test/java/com/tallgeorge/perfect/controllers/HtmlControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,8 +22,6 @@ public class HtmlControllerTest {
 
     @Test
     public void testIndex() throws Exception {
-        this.mockMvc.perform(get("/").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"));
+        this.mockMvc.perform(get("/")).andExpect(status().isOk());
     }
 }
